d40ae47fee1ec615aec5ba597e67ec4198a9ebce
rakeenzaman <160088078+rakeenzaman@users.noreply.github.com>
2024-04-13 19:18:16 -0500

    Update input.cpp

--- input.cpp
+++ input.cpp
@@ -1,12 +1,1 @@
-string escapeDoubleQuotes(const string& input) {
-    string output;
 
-    for (char ch : input) {
-        if (ch == '\"') {
-            output += "\\\"";
-        } else {
-            output += ch;
-        }
-    }
-    return output;
-}
